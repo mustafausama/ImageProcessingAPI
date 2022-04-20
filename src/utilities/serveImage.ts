0ce53886595e18bc3e6c711cc8e132eb15f1a0d6
mustafausama <mustafausama@outlook.com>
2022-04-20 08:28:26 +0200

    Fixed Error when image exists

--- src/utilities/serveImage.ts
+++ src/utilities/serveImage.ts
@@ -1,17 +1,19 @@
 import path from 'path';
 import sharp from 'sharp';
-
+import fs from 'fs';
 const serveImage = async (
   filename: string,
   width: string,
   height: string
 ): Promise<void> => {
   try {
-    const cache = await sharp(
-      path.resolve(`assets/thumb/${filename}.thumb.jpg`)
-    ).metadata();
-    if (cache.width === parseInt(width) && cache.height === parseInt(height))
-      return;
+    if (fs.existsSync(path.resolve(`assets/thumb/${filename}.thumb.jpg`))) {
+      const cache = await sharp(
+        path.resolve(`assets/thumb/${filename}.thumb.jpg`)
+      ).metadata();
+      if (cache.width === parseInt(width) && cache.height === parseInt(height))
+        return;
+    }
     const transformer = await sharp(
       path.resolve(`assets/full/${filename}.jpg`)
     ).resize(parseInt(width), parseInt(height));
